1cdc4f98b1cd4c51de1aca773e0e54ef831cb598
Wupei <rablwupei@gmail.com>
2020-03-30 23:04:42 +0800

    add zhongzhengzhishu

---
+++ inv_express/inv/excel/parser/Zhongzhengzhishu.js
@@ -0,0 +1,43 @@
+let DataSourceParser = require("../DataSourceParser");
+
+class DataSourceZhongzhengzhishu extends DataSourceParser {
+    get key() {
+        return "中证指数";
+    }
+
+    get regular() {
+        return /中证指数\[(.*?)\]\[(.*?)\]/g;
+    }
+
+    replaceStr(str) {
+        return str.replace(this.regular, 'values["中证指数"]["$1"]["$2"]')
+    }
+
+    *request() {
+        if (this._ids.size === 0) {
+            return;
+        }
+        var requests = [];
+        var market = require('../../market/zhongzhengzhishu');
+        var ids = Array.from(this._ids);
+        for (let i = 0; i < ids.length; i++) {
+            var code = ids[i];
+            requests.push(market.get(code));
+        }
+        this._stocks = yield requests;
+    }
+
+    fillValue(values) {
+        if (this._ids.size === 0) {
+            return;
+        }
+        var value = {};
+        values[this.key] = value;
+        for (let i = 0; i < this._stocks.length; i++) {
+            var stock = this._stocks[i];
+            value["" + stock.code] = stock.json;
+        }
+    }
+}
+
+module.exports = DataSourceZhongzhengzhishu;
---
+++ inv_express/inv/market/zhongzhengzhishu.js
@@ -0,0 +1,52 @@
+let AbstractStock = require('./AbstractStock');
+let util = require('util');
+
+class ZhongzhengzhishuStock extends AbstractStock {
+    constructor(code) {
+        super(code);
+    }
+
+    convert(value) {
+        value = value.replaceAll(",", "");
+        value = value.replaceAll("%", "");
+        return value;
+    }
+
+    parse(text) {
+        const cheerio = require('cheerio');
+        const $ = cheerio.load(text);
+        let that = this;
+        var json = {};
+        $("table[class='table tc']").find("tbody").children().each(function (index, elem) {
+            $(elem).children().each(function (index, subElem) {
+                json[index] = that.convert($(subElem).text());
+            });
+            var num = parseFloat(json[1]);
+            if (!isNaN(num)) {
+                json[1] = num / 100;
+            }
+        });
+        this.json = json;
+    }
+}
+
+var http = require('../utils/http');
+var m = {};
+
+m.url = "http://www.csindex.com.cn/zh-CN/indices/index-detail/%s";
+
+m.get = function*(code) {
+    var url = util.format(m.url, code);
+    var referer = url;
+    console.log(url)
+    var body = yield http.get(url, {headers: {'Referer': referer} });
+    var stock = new ZhongzhengzhishuStock(code);
+    stock.parse(body);
+    return stock;
+};
+//中证指数[H11136][2]
+// require('co')(function* () {
+//     yield m.get("H11136");
+// });
+
+module.exports = m;
