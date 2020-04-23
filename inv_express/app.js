a21a8c4efb7464412bacecf8bd4de13280aeb9ed
rablwupei <rablwupei@gmail.com>
2020-04-23 12:55:48 +0800

    env

--- inv_express/app.js
+++ inv_express/app.js
@@ -57,7 +57,7 @@ passport.deserializeUser(function(user, done) {
 app.use('/login', require('./routes/login'));
 
 app.use((req, res, next) => {
-    if (req.get('host').indexOf('localhost') >= 0 || req.isAuthenticated()) {
+    if (process.env.NODE_ENV === 'production' || req.isAuthenticated()) {
         return next()
     }
     req.session.returnTo = req.originalUrl;
