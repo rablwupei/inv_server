34c530bcb9f7490b9c6fd93bc666a8e52f7ee791
rablwupei <rablwupei@gmail.com>
2020-04-27 13:31:50 +0800

    tips

--- inv_express/inv/miaomiao/Result.js
+++ inv_express/inv/miaomiao/Result.js
@@ -17,22 +17,37 @@ class Result {
         var offsetStr = "";
         var tips = "";
         var tipsClass = null;
+        var tips2Class = null;
         var tips2 = "";
         if (unit.tips) {
             tips = unit.tips
         }
+        let total = 50;
         if (unit.price1 > 0) {
-            //0.0 0.2  10% 3.3%
-            //0.1 0.2  10% 6.6%
-            //0.2 0.2  10% 10%
             if (unit.cangwei) {
                 if (offset < 0) {
+                    //0.0 0.2  10% 3.3%
+                    //0.1 0.2  10% 6.6%
+                    //0.2 0.2  10% 10%
                     var percent = ((-offset + 0.1) / 0.3) * unit.cangwei;
                     tips = sprintf("%.2f%%", percent * 100);
-                    tips2 = sprintf("%.2f", percent * 50);
+                    tips2 = sprintf("%.2f", percent * total);
                 } else {
-                    tips = sprintf("%.2f%%", unit.cangwei * 100);
-                    tipsClass = "gray";
+                    //0.0 0.1  10% 10%
+                    //0.05 0.1  10% 5%
+                    //0.1 0.1  10% 0%
+                    var percent = (unit.cangwei / 3 * ((0.1 - offset) / 0.1));
+                    if (percent > 0) {
+                        tips = sprintf("%.2f%%", percent * 100);
+                        tips2 = sprintf("%.2f", percent * total);
+                        tipsClass = "gray";
+                        tips2Class = "gray";
+                    } else {
+                        tips = sprintf("%.2f%%", unit.cangwei * 100);
+                        tips2 = 0;
+                        tipsClass = "gray";
+                        tips2Class = "gray";
+                    }
                 }
             }
             offsetStr = sprintf("%.2f%%", offset * 100);
@@ -46,7 +61,7 @@ class Result {
             {text:unit.price1},
             {text:offsetStr},
             {text:tips, class:tipsClass},
-            {text:tips2},
+            {text:tips2, class:tips2Class},
         ]
     }
 
