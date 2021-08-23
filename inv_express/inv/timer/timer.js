435b0c7ffa5a30cd45c0f176c46189337e09c1dc
pei_wu <pei.wu@kingsgroupgames.com>
2021-08-23 09:51:58 +0800

    remove xingu

--- inv_express/inv/timer/timer.js
+++ inv_express/inv/timer/timer.js
@@ -35,33 +35,20 @@ timer.start = function () {
         })
     });
 
-    require('../utils/cron').startInTrade('31 9,11,13,14 * * 1-5', async() => {
-        let code = "sh605589";
-        let stockMap = await require('../market/sina').get(code);
-        let stock = stockMap[code];
-        // if (stock.percent < 0.095) {
-        let msg = require('util').format("%s %s(%s%)", stock.name, stock.price, stock.percent * 100);
-        var weixin = require("../utils/weixin");
-        weixin.send({
-            message : msg,
-            touser : "wupei",
-        })
-        // }
-    });
+    // require('../utils/cron').startInTrade('31 9,11,13,14 * * 1-5', async() => {
+    //     let code = "sh605589";
+    //     let stockMap = await require('../market/sina').get(code);
+    //     let stock = stockMap[code];
+    //     // if (stock.percent < 0.095) {
+    //     let msg = require('util').format("%s %s(%s%)", stock.name, stock.price, stock.percent * 100);
+    //     var weixin = require("../utils/weixin");
+    //     weixin.send({
+    //         message : msg,
+    //         touser : "wupei",
+    //     })
+    //     // }
+    // });
 
-    require('../utils/cron').startInTrade('31 9,11,13,14 * * 1-5', async() => {
-        let code = "sh688779";
-        let stockMap = await require('../market/sina').get(code);
-        let stock = stockMap[code];
-        // if (stock.percent < 0.095) {
-        let msg = require('util').format("%s %s(%s%)", stock.name, stock.price, stock.percent * 100);
-        var weixin = require("../utils/weixin");
-        weixin.send({
-            message : msg,
-            touser : "wupei",
-        })
-        // }
-    });
 };
 
 module.exports = timer;
