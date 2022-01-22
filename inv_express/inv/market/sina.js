9031bc17d360a8a5c4ab0a2ca8b41eace48560c3
rablwupei <rablwupei@gmail.com>
2022-01-22 22:14:40 +0800

    sina

--- inv_express/inv/market/sina.js
+++ inv_express/inv/market/sina.js
@@ -26,6 +26,7 @@ class SinaStock extends AbstractStock {
 }
 
 var http = require('../utils/http');
+var util = require("util");
 var sina = {};
 
 sina.url = "http://hq.sinajs.cn/format=text&list=";
@@ -39,7 +40,8 @@ sina.get = async function(codes) {
     // console.log("url: " + url);
     var proxy = require("../db/Proxy");
     var proxyUrl = await proxy.getProxy();
-    var option = {encoding: null, proxy: proxyUrl};
+    var refererUrl = "https://finance.sina.com.cn/realstock/company/sh000001/nc.shtml";
+    var option = {encoding: null, proxy: proxyUrl, headers: {'Referer' : refererUrl}};
     var body = await http.get(url, option);
     body = iconv.decode(body, 'GBK');
     if (!body.endsWith("FAILED")) {
